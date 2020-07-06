6e8421361f9105d6287b3e5266b3d22428ba3370
Alan Fisher <alanmirth@gmail.com>
2020-07-06 09:43:10 +0600

    add … support

--- getSentences.py
+++ getSentences.py
@@ -8,10 +8,11 @@ def getSentences(inputFile, outputFile):
             if len(line.split()) > 4:
                 fullArray.append(line)
         fullString = '' . join(fullArray) . replace('\n', '') . replace(':— ', '.\n')
-        pattern = re.compile(r'([A-ZА-ЯЁ][^\.!?…]*[\.!?])', re.M)
+        pattern = re.compile(r'([A-ZА-ЯЁ][^\.!?…]*[\.!?…])', re.M)
         sentences = pattern.findall(fullString)
         for item in sentences:
             if len(item.split()) > 3:
+                item = item.replace('…', '.')
                 output.write("%s\n" % item)
 
 getSentences('input.txt', 'output.txt')
